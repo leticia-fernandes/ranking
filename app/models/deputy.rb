b33fbe80075e7199bc22e7b6787e0586562d10af
Letícia Fernandes <leticiafs1994@hotmail.com>
2022-10-18 00:53:42 -0300

    Fix total_expenses query

--- app/models/deputy.rb
+++ app/models/deputy.rb
@@ -5,9 +5,13 @@ class Deputy < ApplicationRecord
   has_many :expenses, class_name: 'DeputyExpense', foreign_key: 'deputy_registration', primary_key: 'registration'
 
   scope :total_expenses, -> {
-    select('deputies.*, SUM(value) as total')
-    .left_outer_joins(:expenses)
-    .group('deputy_registration')
-    .order('total DESC')
+    select('deputies.*, t1.total ')
+    .joins('inner join (
+        SELECT deputy_registration as reg, SUM(value) as total
+        FROM "deputies"
+        INNER JOIN "deputy_expenses" ON "deputy_expenses"."deputy_registration" = "deputies"."registration"
+        GROUP BY deputy_registration
+      ) as t1 on deputies.registration = t1.reg'
+    ).order('total DESC')
   }
 end
